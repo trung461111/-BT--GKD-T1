62d44a978822e0b6c5204d7c21402222fd174a96
trung461111 <trungngo046@gmail.com>
2024-04-24 14:34:40 +0700

    ngay

---
+++ BTAP-1.4/NextDate.java
@@ -0,0 +1,19 @@
+package ngay;
+import java.time.LocalDate;
+
+public class NextDate {
+    private int day;
+    private int month;
+    private int year;
+
+    public NextDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public LocalDate getNextDate() {
+        LocalDate date = LocalDate.of(year, month, day);
+        return date.plusDays(1);
+    }
+}
---
+++ BTAP-1.4/NextDateTest.java
@@ -0,0 +1,19 @@
+package ngay;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.LocalDate;
+
+public class NextDateTest {
+    @Test
+    public void testNextDate() {
+        NextDate nextDate = new NextDate(31, 12, 2012);
+        assertEquals(LocalDate.of(2013, 1, 1), nextDate.getNextDate());
+
+        nextDate = new NextDate(28, 2, 2012);
+        assertEquals(LocalDate.of(2012, 2, 29), nextDate.getNextDate());
+
+        nextDate = new NextDate(28, 2, 2011);
+        assertEquals(LocalDate.of(2011, 3, 1), nextDate.getNextDate());
+        
+    }
+}
